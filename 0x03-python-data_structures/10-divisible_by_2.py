86ce7104306e05c7528f4e835f1a31566785f3e2
Tayo Akinrinsoye <tayoakinrinsoye@hotmail.com>
2022-07-05 16:42:24 +0000

    Task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,7 +4,8 @@
 def divisible_by_2(my_list=[]):
     new_list = []
     for num in my_list:
-        if num % 2 == 0;
+        if num % 2 == 0:
             new_list.append(True)
         else:
             new_list.append(False)
+    return new_list
