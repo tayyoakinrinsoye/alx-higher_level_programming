1e96131b87424a379ba883f2093c8188170aa36e
Tayo Akinrinsoye <tayoakinrinsoye@hotmail.com>
2022-07-19 15:18:18 +0000

    Task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+
+
 def safe_print_division(a, b):
     try:
         result = a / b
         print("Inside result: {:.1f}".format(result))
-    except:
+    except Exception:
         result = None
         print("Inside result: {}".format(result))
     finally:
