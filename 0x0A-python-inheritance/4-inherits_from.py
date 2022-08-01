7fc9876e00dff41ddc6093947b83c2fbad93cb2b
Tayo Akinrinsoye <tayoakinrinsoye@hotmail.com>
2022-08-01 22:31:30 +0000

    Task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,6 +2,4 @@
 
 def inherits_from(obj, a_class):
 
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
-        return True
-    return False
+    return isinstance(obj, a_class) and type(obj) != a_class
