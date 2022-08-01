36a104a1444113c4845c8764ef71d607c4c31666
Tayo Akinrinsoye <tayoakinrinsoye@hotmail.com>
2022-08-01 22:46:16 +0000

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Module 6: Base Geometry'''
+
+
+class BaseGeometry():
+    """Represent Base Geometry."""
+
+    def area(self):
+        '''Not Implemented'''
+        raise Exception("area() is not implemented")
