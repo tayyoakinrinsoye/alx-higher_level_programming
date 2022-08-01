d9d01e69e397f24381b59e70786b7d27b9433a4a
Tayo Akinrinsoye <tayoakinrinsoye@hotmail.com>
2022-08-01 22:21:43 +0000

    Task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Module 3-is_kind_of_class
+'''
+
+
+def is_kind_of_class(obj, a_class):
+    ''' function is_kind_of_class
+    obj: an object
+    a_class: a class
+    Returns: Bool
+    '''
+
+    return isinstance(obj, a_class)
