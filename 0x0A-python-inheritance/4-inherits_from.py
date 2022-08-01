9cbe175c2cf3d08d05d032fd65f494786ae5db15
Tayo Akinrinsoye <tayoakinrinsoye@hotmail.com>
2022-08-01 22:33:36 +0000

    Task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+''' MODULE: 4-inherits_from
+'''
+
+
 
 def inherits_from(obj, a_class):
-
+    ''' the object is an instance of a class that inherited (directly or indirectly)
+    obj: an object
+    a_class: a class
+    returns None
+    '''
     return isinstance(obj, a_class) and type(obj) != a_class
