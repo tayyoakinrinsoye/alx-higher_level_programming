47b18e10427fbe7b3cbbcc702bc58d160ac624e0
Tayo Akinrinsoye <tayoakinrinsoye@hotmail.com>
2022-08-02 22:15:36 +0000

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,4 +9,4 @@ def read_file(filename=""):
     Returns none
     """
     with open(filename, "r", encoding="UTF-8") as newfile:
-        print(newfile.read())
+        print(newfile.read(), end="")
