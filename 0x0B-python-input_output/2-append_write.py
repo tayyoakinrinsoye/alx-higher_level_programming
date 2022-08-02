527c7f20942b8768518e74af6c6fdd475f8edaa5
Tayo Akinrinsoye <tayoakinrinsoye@hotmail.com>
2022-08-02 22:26:27 +0000

    Task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+function to append to a file
+"""
+
+def append_write(filename="", text=""):
+    '''function to append a string at the end of a text file
+    '''
+    with open(filename, "a", encoding="UTF-8") as newfile:
+        return newfile.write(text)
