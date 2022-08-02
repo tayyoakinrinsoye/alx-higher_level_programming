8b05b448bd633de5d4e2e594f16f840fe666953e
Tayo Akinrinsoye <tayoakinrinsoye@hotmail.com>
2022-08-02 22:51:13 +0000

    Task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Python to JSON string
+"""
+
+import json
+
+
+def to_json_string(my_obj):
+    '''function that returns JSON representation of an object string'''
+    return json.dumps(my_obj)
