9757b1256df61794a57f7ff3c70cf834b71ab7d6
Tayo Akinrinsoye <tayoakinrinsoye@hotmail.com>
2022-10-29 10:32:12 +0100

    Task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a Pascal's Triangle function."""
+
+
+def pascal_triangle(n):
+    """Represent Pascal's Triangle of size n.
+    Returns a list of lists of integers representing the triangle.
+    """
+    if n <= 0:
+        return []
+
+    list1 = []
+    for i in range(n):
+        temp_list = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                temp_list.append(1)
+            else:
+                temp_list.append(list1[i-1][j-1] + list1[i-1][j])
+        list1.append(temp_list)
+    return list1
