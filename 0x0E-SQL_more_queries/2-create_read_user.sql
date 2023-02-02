f526887f0f59ebe391f705786a284a9ae1c10443
tayo akinrinsoye <tayoakinrinsoye@hotmail.com>
2023-02-02 18:47:35 +0100

    Task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,6 @@
 -- creates a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- create a user 
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED WITH BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 -- grants SELECT privileges to a user
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
