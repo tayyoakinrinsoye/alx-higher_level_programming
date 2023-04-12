8949b38cd969852c996ac686afe024071212884b
Tayo Akinrinsoye <tayoakinrinsoye@hotmail.com>
2023-04-12 18:07:00 +0100

    Task 1 to 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,19 +1,21 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
+    constructor(w, h) {
+        if (w > 0 && h > 0) {
+            this.width = w;
+            this.height = h;
+        }
     }
-  }
 
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      for (let i = 0; i < this.width; i++) {
-        console.log('X');
-      }
-      console.log('/n');
+    print() {
+        let pattern = '';
+        for (let i = 0; i < this.height; i++) {
+            for (let i = 0; i < this.width; i++) {
+                pattern = pattern + 'X';
+            }
+            console.log(pattern);
+            pattern = '';
+        }
     }
-  }
 }
 module.exports = Rectangle;
