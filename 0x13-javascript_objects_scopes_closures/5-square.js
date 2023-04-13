b39c246e955eabf57cf0ffe96c857774225b9387
Tayo Akinrinsoye <tayoakinrinsoye@hotmail.com>
2023-04-13 07:26:43 +0100

    semistandard compliance fix

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // class Square that inherits from Rectangle
 
-Rectangle = require('./4-rectangle');
+const Rectangle = require('./4-rectangle');
 
 const Square = class Square extends Rectangle {
-  constructor (size) {
+  constructor(size) {
     super(size, size);
   }
 };
--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 // function that prints the number of arguments already printed and the new argument value
 exports.logMe = function (item) {
-  let count = 0;
-  console.log(`${count}: ${item}`);
-  count++;
-};
+  let counter = 0;
+  return function () {
+    console.log(`${counter}: ${item}`);
+    counter++;
+  }
+}
