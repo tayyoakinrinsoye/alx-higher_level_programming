c949c385036f84edf85c962db30198aa7c37ef4d
Tayo Akinrinsoye <tayoakinrinsoye@hotmail.com>
2023-04-16 07:55:50 +0100

    TASK 0 and README

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-"""
-     script that lists all states from the database hbtn_0e_0_usa
-"""
+
+
 import MySQLdb
 from sys import argv
 
-
-# Connect to MySQL server running on localhost at port 3306
-db = MySQLdb.connect(host="localhost",
-                     port=3306,
-                     user=argv[1],
-                     passwd=argv[2],
-                     db=argv[3])
-
-cursor = db.cursor()
-
-cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
-
-data = cursor.fetchall()
-for row in data:
-    print(row)
-cursor.close()
-db.close()
+"""
+script that lists all states 
+from the database hbtn_0e_0_usa
+"""
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    data = cursor.fetchall()
+    for row in data:
+        print(row)
+    cursor.close()
+    db.close()
