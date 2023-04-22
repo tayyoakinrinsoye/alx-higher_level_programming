8dc08cba6c1d12cfb21f0b196d84f9b012982eca
Tayo Akinrinsoye <tayoakinrinsoye@hotmail.com>
2023-04-22 13:19:24 +0100

    Task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" prints the State object with the name passed as argument
+from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]))
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter_by(name=argv[4]).first()
+    if not state:
+        print("Not found")
+    else:
+        print(f"{state.id}")
+    session.close()
