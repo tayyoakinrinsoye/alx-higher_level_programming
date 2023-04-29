5838aa2191f789ad0c7f2b50cc7dab32df16344f
Tayo Akinrinsoye <tayoakinrinsoye@hotmail.com>
2023-04-29 20:44:34 +0100

    Task-5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,5 +7,5 @@ import requests
 from sys import argv
 
 if __name__ == '__main__':
-    r = requests.get(argv[1])
-    print(r.headers.get('X-Request-Id'))
+    response = requests.get(argv[1])
+    print(response.headers.get('X-Request-Id'))
