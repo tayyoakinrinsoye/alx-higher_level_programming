cbaaec9fb007f80ccd410b290950313d64cdca54
Tayo Akinrinsoye <tayoakinrinsoye@hotmail.com>
2023-04-29 17:14:15 +0100

    Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,9 +5,9 @@ fetches https://intranet.hbtn.io/status.
 
 
 if __name__ == '__main__':
-    from urllib.request import urlopen
+    import urllib.request
 
-    with urlopen('https://intranet.hbtn.io/status') as resp:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
         content = resp.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
