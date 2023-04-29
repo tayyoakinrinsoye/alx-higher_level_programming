9f6db825a621dd9b52c9f4fddd2831f8baa6a016
Tayo Akinrinsoye <tayoakinrinsoye@hotmail.com>
2023-04-29 18:22:24 +0100

    Task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     try:
         with request.urlopen(sys.argv[1]) as resp:
             print(resp.read().decode('UTF-8'))
-    except error.HTTPError as er:
-        print('Error code:', er.code)
+    except error.HTTPError as err:
+        print('Error code:', err.code)
